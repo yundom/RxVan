9d14a52eb685b976c30600ff8539481dfa8e38ce
Dennis <dennis.hsieh@agoda.com>
2018-06-17 14:55:20 +0700

    Test RxVan.

--- src/test/kotlin/com/yundom/rxvan/RxVanTest.kt
+++ src/test/kotlin/com/yundom/rxvan/RxVanTest.kt
@@ -1,12 +1,48 @@
 package com.yundom.rxvan
 
-import org.junit.Assert.assertTrue
+import io.reactivex.observers.TestObserver
 import org.junit.Test
 
 class RxVanTest {
 
+    data class MyEventType(val data: String)
+
     @Test
-    fun testReceiveEvent() {
-        assertTrue(true)
+    fun testReceiveSingleEvent() {
+        val event = MyEventType("Hello RxVan")
+        val testObserver = TestObserver<MyEventType>()
+        RxVan.get().observe<MyEventType>().subscribe(testObserver)
+
+        RxVan.get().post(event)
+
+        testObserver.assertSubscribed()
+        testObserver.assertNoErrors()
+        testObserver.assertValue {
+            it.data == "Hello RxVan"
+        }
+    }
+
+    @Test
+    fun testReceiveMultipleEvent() {
+        val rounds = 100
+        val events = mutableListOf<MyEventType>()
+        (0 until rounds).forEach {
+            events.add(MyEventType(it.toString()))
+        }
+        val testObserver = TestObserver<MyEventType>()
+        RxVan.get().observe<MyEventType>().subscribe(testObserver)
+
+        events.forEach {
+            RxVan.get().post(it)
+        }
+
+        testObserver.assertSubscribed()
+        testObserver.assertNoErrors()
+        testObserver.assertValueCount(100)
+        (0 until rounds).forEach { round ->
+            testObserver.assertValueAt(round) {
+                it.data == events[round].data
+            }
+        }
     }
 }
